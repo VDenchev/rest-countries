9ce76f714a911563e05675ef0c9fde188c9cf80e
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-08 12:50:52 +0300

    Add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      light: {
+        text: 'hsl(200, 15%, 8%)',
+        input: 'hsl(0, 0%, 52%)',
+        bg: 'hsl(0, 0%, 98%)'
+      },
+      dark: {
+        elements: 'hsl(209, 23%, 22%)',
+        bg: 'hsl(207, 26%, 17%)'
+      }
+    }
   },
   plugins: [],
 }
