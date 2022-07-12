693f411fd93ec291664d040b944555c176dccb84
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-12 13:36:55 +0300

    Create responsive Title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import React from "react"
+import Title from "./Components/Title.jsx"
+
 function App() {
-	return <div className="App"></div>
+	return (
+		<div className="bg-light-bg min-h-screen max-h-fit w-full pb-4">
+			<Title>Where in the world?</Title>
+		</div>
+	)
 }
 
 export default App
---
+++ src/Components/Title.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon as darkMoon } from "@fortawesome/free-solid-svg-icons"
+import { faMoon as lightMoon } from "@fortawesome/free-regular-svg-icons"
+
+const Title = ({ children }) => {
+	return (
+		<header className="flex items-center justify-between h-20 px-[clamp(1rem,6.01vw-0.41rem,5rem)] bg-white text-light-text">
+			<h1 className="text-[clamp(0.88rem,0.94vw+0.65rem,1.5rem)] font-extrabold">
+				{children}
+			</h1>
+			<div className="flex items-stretch gap-2">
+				<FontAwesomeIcon
+					icon={lightMoon}
+					className="h-[clamp(1rem,0.56vw+0.87rem,1.38rem)]"
+				/>
+				<p className="text-[clamp(0.75rem,0.38vw+0.66rem,1rem)] font-semibold">
+					Dark Mode
+				</p>
+			</div>
+		</header>
+	)
+}
+
+export default Title
