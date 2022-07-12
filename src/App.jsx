ac73b46c78abc61ce53be55fd12a162efc35048a
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-12 15:50:37 +0300

    Add static Countries page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Title from "./Components/Title.jsx"
+import Countries from "./Pages/Countries.jsx"
 
 function App() {
 	return (
-		<div className="bg-light-bg min-h-screen max-h-fit w-full pb-4">
+		<div className="bg-light-bg min-h-screen max-h-fit w-full">
 			<Title>Where in the world?</Title>
+			<main className="px-[clamp(1rem,6.01vw-0.41rem,5rem)] py-[clamp(1.5rem,2.25vw+0.97rem,3rem)]">
+				<Countries />
+			</main>
 		</div>
 	)
 }
---
+++ src/Pages/Countries.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "../Components/Card.jsx"
+
+const Countries = () => {
+	return (
+		<div className="flex flex-wrap justify-center sm:justify-start gap-10 md:gap-16">
+			<Card />
+			<Card />
+			<Card />
+		</div>
+	)
+}
+
+export default Countries
