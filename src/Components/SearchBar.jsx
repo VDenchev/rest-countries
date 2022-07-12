baad15c766ad29f4c18e62f14ec22fcb3af7e295
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-12 16:43:18 +0300

    Add a SearchBar component

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+const SearchBar = () => {
+	return (
+		<div className="px-9 h-12 sm:h-14 flex w-full sm:w-[33.3vw] gap-6 items-center bg-white rounded-md shadow-sm">
+			<FontAwesomeIcon icon={faMagnifyingGlass} className="text-light-input" />
+			<input
+				type="text"
+				name=""
+				id=""
+				className="text-xl font-semibold text-light-input bg-transparent text-[clamp(0.75rem,0.19vw+0.71rem,0.88rem)] h-full focus:outline-none w-full"
+				placeholder="Search for a country... "
+			/>
+		</div>
+	)
+}
+
+export default SearchBar
--- src/Pages/Countries.jsx
+++ src/Pages/Countries.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import Card from "../Components/Card.jsx"
+import SearchBar from "../Components/SearchBar.jsx"
 
 const Countries = () => {
 	return (
-		<div className="flex flex-wrap justify-center sm:justify-start gap-10 md:gap-16">
-			<Card />
-			<Card />
-			<Card />
-		</div>
+		<>
+			<div className="flex flex-col justify-between items-center sm:flex-row gap-10 mb-[clamp(2rem,1.5vw+1.65rem,3rem)]">
+				<SearchBar />
+			</div>
+			<div className="flex flex-wrap justify-center sm:justify-start gap-10 md:gap-16">
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</>
 	)
 }
 
