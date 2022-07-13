5834096f06b734a518fd7191dd5160a3da51c1b8
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-13 21:58:40 +0300

    Add route for country page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Countries from "./Pages/Countries.jsx"
+import Country from "./Pages/Country.jsx"
 import Home from "./Pages/Home.jsx"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="countries" element={<Home />}>
 					<Route index element={<Countries />} />
+					<Route path=":countryName" element={<Country />} />
 				</Route>
 				<Route path="/" element={<Navigate to="/countries" replace={true} />} />
 			</Routes>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Card = ({ flag, name, population, region, capital }) => {
 	return (
 		<section className="bg-white shadow-sm w-[264px] h-[336px] rounded-md overflow-hidden">
-			<img src={flag} alt="" className="max-h-40 w-full shadow-sm" />
+			<Link to={`./${name}`}>
+				<img src={flag} alt="" className="max-h-40 w-full shadow-sm" />
+			</Link>
 			<div className="p-6 text-light-text">
 				<h2 className="font-extrabold text-lg pb-4">{name}</h2>
 				<p className="text-sm font-semibold pb-2">
