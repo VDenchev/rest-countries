6d16fc8b99a2ccda1828e396c0a88c224445fc3c
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-13 12:21:53 +0300

    Move code from App into Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react"
-import Title from "./Components/Title.jsx"
-import Countries from "./Pages/Countries.jsx"
+import Home from "./Pages/Home.jsx"
 
 function App() {
 	return (
-		<div className="bg-light-bg min-h-screen max-h-fit w-full">
-			<Title>Where in the world?</Title>
-			<main className="px-[clamp(1rem,6.01vw-0.41rem,5rem)] py-[clamp(1.5rem,2.25vw+0.97rem,3rem)]">
-				<Countries />
-			</main>
+		<div>
+			<Home />
 		</div>
 	)
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Title from "../Components/Title.jsx"
+import Countries from "./Countries.jsx"
+
+const Home = () => {
+	return (
+		<div className="bg-light-bg min-h-screen max-h-fit w-full">
+			<Title>Where in the world?</Title>
+			<main className="px-[clamp(1rem,6.01vw-0.41rem,5rem)] py-[clamp(1.5rem,2.25vw+0.97rem,3rem)]">
+				<Countries />
+			</main>
+		</div>
+	)
+}
+
+export default Home
