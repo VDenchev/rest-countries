296996e3b1edcbd9a8abd6f57e5e1c0effd0dbb4
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-14 13:10:47 +0300

    Fetch data from api for a single country and display it

--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,34 +1,77 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import Back from "../Components/Back.jsx"
 import Button from "../Components/Button.jsx"
 import Detail from "../Components/Detail.jsx"
 
 const Country = () => {
+	const [countryData, setCountryData] = useState()
+
+	const params = useParams()
+	useEffect(() => {
+		const getCountryData = async () => {
+			const data = await fetch(
+				`https://restcountries.com/v3.1/name/${params.countryName}?fullText=true`
+			)
+			console.log(data)
+			const json = await data.json()
+			const obj = await json.at(0)
+			setCountryData(obj)
+		}
+		getCountryData().catch(console.error)
+	}, [])
+
+	console.log(countryData)
 	return (
 		<div className="pt-[clamp(1rem,1.5vw+0.65rem,2rem)]">
 			<Back to="/">Back</Back>
 			<div className="sm:flex sm:gap-[10%] sm:items-center mt-[clamp(4rem,1.5vw+3.65rem,5rem)]">
 				<img
-					src="https://via.placeholder.com/500"
+					src={countryData?.flags?.svg}
 					alt=""
 					className="w-full sm:w-[40vw]"
 				/>
 				<div className="text-light-text mt-11 lg:flex-1">
 					<h2 className="text-[clamp(1.38rem,0.94vw+1.15rem,2rem)] mb-6 font-extrabold">
-						Belgium
+						{countryData?.name?.common}
 					</h2>
 					<div className="lg:flex lg:justify-between">
 						<div className="mb-11">
-							<Detail name="Native Name" value="Belgie"></Detail>
-							<Detail name="Population" value="11,319,511"></Detail>
-							<Detail name="Region" value="Europe"></Detail>
-							<Detail name="Sub Region" value="Western Europe"></Detail>
-							<Detail name="Capital" value="Brussels"></Detail>
+							<Detail
+								name="Native Name"
+								value={
+									countryData &&
+									Object.values(countryData?.name?.nativeName)
+										.map((name) => name.common)
+										.join(", ")
+								}
+							/>
+							<Detail name="Population" value={countryData?.population} />
+							<Detail name="Region" value={countryData?.region} />
+							<Detail name="Sub Region" value={countryData?.subregion} />
+							<Detail name="Capital" value={countryData?.capital} />
 						</div>
 						<div>
-							<Detail name="Top Level Domain" value=".be"></Detail>
-							<Detail name="Currencies" value="Euro"></Detail>
-							<Detail name="Languages" value="Dutch, French, German"></Detail>
+							<Detail
+								name="Top Level Domain"
+								value={countryData?.tld.join(", ")}
+							/>
+							<Detail
+								name="Currencies"
+								value={
+									countryData &&
+									Object.values(countryData?.currencies)
+										.map((cur) => cur.name)
+										.join(", ")
+								}
+							/>
+							<Detail
+								name="Languages"
+								value={
+									countryData &&
+									Object.values(countryData?.languages).join(", ")
+								}
+							/>
 						</div>
 					</div>
 					<div>
