2ded3c258c82b32ab3191c7dd067aefe85e38ccb
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-21 22:13:47 +0300

    Add ability to search countries by name using query parameters

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-const SearchBar = () => {
+const SearchBar = ({ handleChange, value }) => {
 	return (
 		<div className="px-9 h-12 sm:h-14 flex w-full sm:w-[33.3vw] gap-6 items-center bg-white rounded-md shadow-sm">
 			<FontAwesomeIcon icon={faMagnifyingGlass} className="text-light-input" />
 			<input
 				type="text"
+				onChange={handleChange}
+				value={value}
 				name=""
 				id=""
 				className="text-xl font-semibold text-light-input bg-transparent text-[clamp(0.75rem,0.19vw+0.71rem,0.88rem)] h-full focus:outline-none w-full"
--- src/Pages/Countries.jsx
+++ src/Pages/Countries.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
 import Card from "../Components/Card.jsx"
 import SearchBar from "../Components/SearchBar.jsx"
 import Spinner from "../Components/Spinner.jsx"
+import { useDebounce } from "../Hooks/useDebounce.js"
 
 const Countries = () => {
 	const [countries, setCountries] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
+	const [searchParams, setSearchParams] = useSearchParams()
+	const [search, setSearch] = useState(searchParams.get("search"))
+	let debounce = useDebounce(search, 500)
+	useEffect(() => {
+		setSearchParams(debounce ? { search: debounce } : {})
+	}, [debounce])
+
 	useEffect(() => {
 		const fetchCountries = async () => {
-			const data = await fetch("https://restcountries.com/v3.1/all")
+			const data = await fetch(
+				searchParams.get("search")
+					? `https://restcountries.com/v3.1/name/${searchParams.get("search")}`
+					: "https://restcountries.com/v3.1/all"
+			)
 			const json = await data.json()
 			setCountries(json)
 			setIsLoading(false)
 			console.log(json)
 		}
 		fetchCountries().catch(console.error)
-	}, [])
+	}, [searchParams])
 
 	const mapCountries = (countries) => {
 		return countries.map((country) => (
@@ -38,7 +51,10 @@ const Countries = () => {
 	return (
 		<>
 			<div className="flex flex-col justify-between items-center sm:flex-row gap-10 mb-[clamp(2rem,1.5vw+1.65rem,3rem)]">
-				<SearchBar />
+				<SearchBar
+					handleChange={(e) => setSearch(e.target.value)}
+					value={search}
+				/>
 			</div>
 			<div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-5 md:gap-10 place-items-center">
 				{isLoading ? <Spinner width={70} /> : cards}
