c78d703682401f5493ce04bb964e74c2e59073a7
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-21 14:41:26 +0300

    Make title a home button

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -2,13 +2,16 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMoon as darkMoon } from "@fortawesome/free-solid-svg-icons"
 import { faMoon as lightMoon } from "@fortawesome/free-regular-svg-icons"
+import { Link } from "react-router-dom"
 
 const Title = ({ children }) => {
 	return (
 		<header className="flex items-center justify-between h-20 px-[clamp(1rem,6.01vw-0.41rem,5rem)] bg-white text-light-text">
-			<h1 className="text-[clamp(0.88rem,0.94vw+0.65rem,1.5rem)] font-extrabold">
-				{children}
-			</h1>
+			<Link to="/countries">
+				<h1 className="text-[clamp(0.88rem,0.94vw+0.65rem,1.5rem)] font-extrabold">
+					{children}
+				</h1>
+			</Link>
 			<div className="flex items-stretch gap-2">
 				<FontAwesomeIcon
 					icon={lightMoon}
